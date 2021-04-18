3123277ee1429e786f9793bb8ecdb9fc8b226de3
Aarush Vailaya <77907708+goombamaui@users.noreply.github.com>
2021-04-18 11:19:56 -0700

    Update main.js
    Making extension more formal

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var xhr = new XMLHttpRequest();
 var but=document.getElementsByClassName("_1SIMq _2kpZl _3OAXJ _13cCs _3_bfp _2M5aC _24avl _3v0y7 _2s0LQ _3ghFm _3LeCL _31GLY _9GDcm _1D8fw util-height-six-3PHnk Header-header-button-active-state-3AvBm Header-header-button-1EE8Y Z_KgC fjQuT uQOmx")[0];
 
 function k(){
-	console.log("get a life")
 	var userMenu=document.getElementsByClassName("_2T2dA Header-header-drop-menu-3SaYV util-min-width-twenty-two-2a0Y- util-max-width-twenty-six-1OJjn _38KgL _35hYo _2mWUT _2ue1O les2- util-box-shadow-dropdown-2Bl9b util-margin-top-negative-point-four-3GRLY _3Xw3k _2trRU j17AQ S42JQ VSOiH _3RmDr fjQuT uQOmx")[0];
 
 	if (userMenu==undefined){
