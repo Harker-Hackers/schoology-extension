703f9b20c981984a9ad1734ef52576f477f8e57c
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-04-19 08:21:38 -0700

    Add rate limiting to downloading server

--- download/main.py
+++ download/main.py
@@ -1,9 +1,16 @@
 import flask
 from os import getenv
 from rockset import Client
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = flask.Flask(__name__)
 collection = Client(api_key=getenv("RS2_TOKEN"), api_server="api.rs2.usw2.rockset.com").Collection.retrieve("schoology-extension-downloads")
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["30 per day"]
+)
 
 @app.route("/")
 def main():
