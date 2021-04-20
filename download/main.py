bfd45138d5ca838a64e021b8b48e46a7fa9baca5
Aarav Borthakur <69025547+gadhagod@users.noreply.github.com>
2021-04-20 09:58:15 -0700

    Add dashboard route to download server (for admins only) (#14)
    * Add dashboard route (for admins only)
    
    * Remove server exception
    
    * Add Flask-Limiter dependency to download server

--- download/main.py
+++ download/main.py
@@ -1,11 +1,12 @@
 import flask
 from os import getenv
-from rockset import Client
+from rockset import Client, Q
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 app = flask.Flask(__name__)
-collection = Client(api_key=getenv("RS2_TOKEN"), api_server="api.rs2.usw2.rockset.com").Collection.retrieve("schoology-extension-downloads")
+rs = Client(api_key=getenv("RS2_TOKEN"), api_server="api.rs2.usw2.rockset.com")
+collection = rs.Collection.retrieve("schoology-extension-downloads")
 limiter = Limiter(
     app,
     key_func=get_remote_address,
@@ -16,9 +17,18 @@ limiter = Limiter(
 def main():
     return flask.redirect("https://github.com/Harker-Hackers/schoology-extension/archive/refs/heads/master.zip")
 
+@app.route("/dashboard")
+def dashboard():
+    if flask.request.args.get("token") == getenv("DASHBOARD_TOKEN"):
+        return {"data": list(rs.sql(Q("select _event_time from \"schoology-extension-downloads\"")))}
+    else:
+        return main()
+
 @app.after_request
-def hits(response):
-    collection.add_docs([{}])
+def log(response):
+    if response._status_code == 302:
+        collection.add_docs([{}])
+    print(response._status)
     return(response)
 
 if __name__ == "__main__":
