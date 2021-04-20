901bd53c93922b06d61cab488533317ccb620661
Aarush Vailaya <77907708+goombamaui@users.noreply.github.com>
2021-04-20 09:57:57 -0700

    Update pdfview.js

--- pdfview.js
+++ pdfview.js
@@ -1,12 +1,16 @@
-// Get PDF link
-inside_span = document.getElementsByClassName("attachments-file-name")[0].innerHTML
+try{
+    // Get PDF link
+    inside_span = document.getElementsByClassName("attachments-file-name")[0].innerHTML
 
-start = inside_span.indexOf("<a href=\"") + 9
-end = inside_span.indexOf(">") - 28
-link = inside_span.slice(start, end)
+    start = inside_span.indexOf("<a href=\"") + 9
+    end = inside_span.indexOf(">") - 28
+    link = inside_span.slice(start, end)
 
-// Change iframe
-document.getElementsByClassName("docviewer-iframe")[0].setAttribute("src", link)
+    if (link.slice(-4)!=".pdf"){throw Error}
 
-// Change "view" button
-view_button = document.getElementsByClassName("view-file-popup ")[0].setAttribute("href", link);
+    // Change iframe
+    document.getElementsByClassName("docviewer-iframe")[0].setAttribute("src", link)
+
+    // Change "view" button
+    view_button = document.getElementsByClassName("view-file-popup ")[0].setAttribute("href", link);
+} catch(err){}
