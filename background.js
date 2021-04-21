c4217471338cf8841d97d4ab06e09336bcd9a2f4
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-04-21 12:46:47 -0700

    Format all files

--- background.js
+++ background.js
@@ -1,49 +1,58 @@
-chrome.runtime.onMessage.addListener(
-    function(data, sender, onSuccess) {
-		if (data.type!=="url"){return};
-		console.log("get");
-		var url = data.url;
-		if (data.cors==false){var mode ="no-cors"} else {var mode = "cors"};
-        fetch(url, {method:"get", mode:mode})
-            .then(response => response.text())
-            .then(responseText => onSuccess(responseText))
-        
-        return true;  // Will respond asynchronously.
+chrome.runtime.onMessage.addListener(function (data, sender, onSuccess) {
+    if (data.type !== "url") {
+        return;
     }
-);
-chrome.runtime.onMessage.addListener(
-    function(data, sender, onSuccess) {
-		if (data.type!=="urlPost"){return};
-		console.log("post");
-		var url = data.url;
-		if (data.formData!==undefined){
-		var formData = new FormData();
-		var pData=data.formData;
-		for (i in pData){
-			formData.append(i, pData[i]);
-		};
-		if (data.cors=false){
-		console.log(formData);
-        fetch(url, {method:"POST", body:formData, mode:"no-cors"})
-            .then(response => response.text())
-            .then(responseText => onSuccess(responseText))
-        } else {
-		console.log(formData);
-        fetch(url, {method:"POST", body:formData})
-            .then(response => response.text())
-            .then(responseText => onSuccess(responseText))
-		}
-        return true;  // Will respond asynchronously.
-		} else if (data.rawData!==undefined){
-		var pData=data.rawData;
-		if (data.cors=false){
-        fetch(url, {method:"POST", body:new URLSearchParams(pData), mode:"no-cors"})
-            .then(response => onSuccess(response))
-        } else {
-		fetch(url, {method:"POST", body:new URLSearchParams(pData)})
-            .then(response => onSuccess(response))
-		}
-        return true;  // Will respond asynchronously.
-		}
+    console.log("get");
+    var url = data.url;
+    if (data.cors == false) {
+        var mode = "no-cors";
+    } else {
+        var mode = "cors";
     }
-);
+    fetch(url, { method: "get", mode: mode })
+        .then((response) => response.text())
+        .then((responseText) => onSuccess(responseText));
+
+    return true; // Will respond asynchronously.
+});
+chrome.runtime.onMessage.addListener(function (data, sender, onSuccess) {
+    if (data.type !== "urlPost") {
+        return;
+    }
+    console.log("post");
+    var url = data.url;
+    if (data.formData !== undefined) {
+        var formData = new FormData();
+        var pData = data.formData;
+        for (i in pData) {
+            formData.append(i, pData[i]);
+        }
+        if ((data.cors = false)) {
+            console.log(formData);
+            fetch(url, { method: "POST", body: formData, mode: "no-cors" })
+                .then((response) => response.text())
+                .then((responseText) => onSuccess(responseText));
+        } else {
+            console.log(formData);
+            fetch(url, { method: "POST", body: formData })
+                .then((response) => response.text())
+                .then((responseText) => onSuccess(responseText));
+        }
+        return true; // Will respond asynchronously.
+    } else if (data.rawData !== undefined) {
+        var pData = data.rawData;
+        if ((data.cors = false)) {
+            fetch(url, {
+                method: "POST",
+                body: new URLSearchParams(pData),
+                mode: "no-cors",
+            }).then((response) => onSuccess(response));
+        } else {
+            fetch(url, {
+                method: "POST",
+                body: new URLSearchParams(pData),
+            }).then((response) => onSuccess(response));
+        }
+        return true; // Will respond asynchronously.
+    }
+});
