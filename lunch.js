48e92717560751d140adb785e4c6acbef51d115e
Aarush Vailaya <77907708+goombamaui@users.noreply.github.com>
2021-04-22 12:54:04 -0700

    Update lunch.js
    Lunch.js now properly fetches pdf

--- lunch.js
+++ lunch.js
@@ -1,11 +1,16 @@
-fetch('https://resources.harker.org/?wpdmpro=middle-school-lunch-menu').then(r => r.text()).then(result => {
+chrome.runtime.sendMessage(
+	{
+		type: "url",
+		url:"https://resources.harker.org/?wpdmpro=middle-school-lunch-menu'",
+	},
+	(result) => {
     start = result.indexOf("btn-sm' href=") + 14
     end = result.indexOf(">Download</a>&nbsp;") - 19
         
     pdf_link = result.slice(start, end)
     console.log(pdf_link)
 
-    document.getElementById("main-content-wrapper").innerHTML = `<center>
-        <iframe src="${pdf_link}" type="application/pdf"</iframe>
-        </center>`
-})
+    document.getElementById("main-content-wrapper").innerHTML = `
+        <iframe src="${pdf_link}" type="application/pdf"></iframe>`
+	}
+);
