01c3b4dfb50c042e0af0ca1d5b6667c9d18fab62
AdrianR3 <93000948+AdrianR3@users.noreply.github.com>
2022-09-08 10:25:13 -0700

    Simplify lunch.js
    Removed unused code

--- lunch.js
+++ lunch.js
@@ -1,21 +1,8 @@
 document.title = "Lunch";
-chrome.runtime.sendMessage(
-    {
-        type: "url",
-        url: "https://resources.harker.org/?wpdmpro=middle-school-lunch-menu'",
-    },
-    (result) => {
-        start = result.indexOf("btn-sm' href=") + 14;
-        end = result.indexOf(">Download</a>&nbsp;") - 19;
 
-        pdf_link = result.slice(start, end);
-        console.log(pdf_link);
-
-        document.getElementById("wrapper").innerHTML = `
+document.getElementById("wrapper").innerHTML = `
     <br>
     <center>
         <iframe src="${"https://resources.harker.org/download/middle-school-lunch-menu/?wpdmdl=130"}" height=700 width=80% style="border: 1px solid black"></iframe>
     </center>
-    `;
-    }
-);
+`;
